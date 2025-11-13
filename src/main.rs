e296f0a70383cc892ed9ea87ae5abd546e3fea56
Masaya Kojima <ac2393921@gmail.com>
2025-11-13 19:58:16 +0900

    feat: CPUを作成

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,13 @@
+#[derive(Default)]
 struct Registers {
     a: u8,
     b: u8,
     c: u8,
     d: u8,
     e: u8,
-    f: u8,
+    f: FlagsRegister,
     h: u8,
     l: u8,
-}
-
-impl Registers {
-    pub fn new() -> Self {
-        Self {
-            a: 0,
-            b: 0,
-            c: 0,
-            d: 0,
-            e: 0,
-            f: 0,
-            h: 0,
-            l: 0,
-        }
-    }
 }
 
 impl Registers {
@@ -41,7 +27,7 @@ impl Registers {
     }
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(Default,PartialEq, Debug)]
 struct FlagsRegister {
     zero: bool,
     subtract: bool,
@@ -79,13 +65,66 @@ impl std::convert::From<u8> for FlagsRegister {
     }
 }
 
+// すべての命令が定義される中心的な場所
+enum Instruction {
+    ADD(ArithmeticTarget),
+}
+
+enum ArithmeticTarget {
+    A,
+    B,
+    C,
+    D,
+    E,
+    H,
+    L,
+}
+
+#[derive(Default)]
+struct CPU {
+    registers: Registers,
+}
+
+impl CPU {
+    fn execute(&mut self, instruction: Instruction) {
+        match instruction {
+            Instruction::ADD(target) => match target {
+                ArithmeticTarget::A => {}
+                ArithmeticTarget::B => {}
+                ArithmeticTarget::C => {
+                    let value = self.registers.c;
+                    let new_value = self.add(value);
+                    self.registers.a = new_value;
+                }
+                ArithmeticTarget::D => {}
+                ArithmeticTarget::E => {}
+                ArithmeticTarget::H => {}
+                ArithmeticTarget::L => {}
+            },
+        }
+    }
+
+    fn add(&mut self, value: u8) -> u8 {
+        let (new_value, did_overflow) = self.registers.a.overflowing_add(value);
+        self.registers.f.zero = new_value == 0;
+        // addなのでsubtractはfalse
+        self.registers.f.subtract = false;
+        // オーバーフローが発生したらcarryはtrue
+        self.registers.f.carry = did_overflow;
+        //下位ニブルの和が0xFを超えたらhalf_carryはtrue
+        // 上位ニブルをマスクして0xFと論理和を取り、0xFを超えたらtrue
+        self.registers.f.half_carry = (self.registers.a & 0xF) + (value & 0xF) > 0xF;
+        new_value
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_get_bc() {
-        let mut registers = Registers::new();
+        let mut registers = Registers::default();
         registers.b = 0x1A;
         registers.c = 0x3C;
         assert_eq!(registers.get_bc(), 0x1A3C);
@@ -93,7 +132,7 @@ mod tests {
 
     #[test]
     fn test_set_bc() {
-        let mut registers = Registers::new();
+        let mut registers = Registers::default();
         registers.set_bc(0x1A3C);
         assert_eq!(registers.b, 0x1A);
         assert_eq!(registers.c, 0x3C);
@@ -101,14 +140,57 @@ mod tests {
 
     #[test]
     fn test_flags_register_from_u8() {
-        let flag = FlagsRegister { zero: true, subtract: false, half_carry: true, carry: false };
+        let flag = FlagsRegister {
+            zero: true,
+            subtract: false,
+            half_carry: true,
+            carry: false,
+        };
         assert_eq!(u8::from(flag), 0b10100000);
     }
 
     #[test]
     fn test_u8_from_flags_register() {
         let u8_value = 0b10100000;
-        assert_eq!(FlagsRegister::from(u8_value), FlagsRegister { zero: true, subtract: false, half_carry: true, carry: false });
+        assert_eq!(
+            FlagsRegister::from(u8_value),
+            FlagsRegister {
+                zero: true,
+                subtract: false,
+                half_carry: true,
+                carry: false
+            }
+        );
+    }
+
+    // addでオーバーフローが発生しない場合のテスト
+    #[test]
+    fn test_add_no_overflow() {
+        let mut cpu = CPU::default();
+        cpu.registers.a = 0x01;
+        cpu.registers.c = 0x02;
+        cpu.execute(Instruction::ADD(ArithmeticTarget::C));
+        assert_eq!(cpu.registers.a, 0x03);
+    }
+
+    // addでオーバーフローが発生する場合のテスト
+    #[test]
+    fn test_add_overflow() {
+        let mut cpu = CPU::default();
+        cpu.registers.a = 0xFF;
+        cpu.registers.c = 0x01;
+        cpu.execute(Instruction::ADD(ArithmeticTarget::C));
+        assert_eq!(cpu.registers.a, 0x00);
+    }
+
+    // addでhalf_carryが発生する場合のテスト
+    #[test]
+    fn test_add_half_carry() {
+        let mut cpu = CPU::default();
+        cpu.registers.a = 0x0F;
+        cpu.registers.c = 0x01;
+        cpu.execute(Instruction::ADD(ArithmeticTarget::C));
+        assert_eq!(cpu.registers.a, 0x10);
     }
 }
 
